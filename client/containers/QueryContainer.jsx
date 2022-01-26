1c8f426dfb2678d81ecad22513bd2666c6359a21
Alex Hersler <ahersler@gmail.com>
2022-01-26 12:55:44 -0800

    cannot for the life of me get my data to arrive at my front end

--- client/containers/QueryContainer.jsx
+++ client/containers/QueryContainer.jsx
@@ -3,18 +3,55 @@ import React, { Component } from 'react';
 class QueryContainer extends Component {
   constructor(props) {
     super();
+    this.handleQueryClick = this.handleQueryClick.bind(this);
   }
 
-  handleQueryClick() {
-    fetch('http://localhost:3000/query').then(res => res.json()).then(data => {
-        console.log('back at front end');
-        console.log(data);
-      }).catch(err => {
-      console.log('ERROR FRONT END')
-      console.log(err)
-  }
-      )
-  }
+   handleQueryClick() {
+    // try {
+    fetch('http://localhost:3000/query', {
+      method: 'GET',
+      // header: {
+      //   'Access-Control-Allow-Origin': 'http://localhost:3000' 
+      // }
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: 'no-cors',
+    })
+    .then(res => {
+      console.log('back at front end', Date.now());
+      console.log(res);
+      res.json();
+    })
+    .then(data => {
+      console.log('after parse')
+      console.log(data);
+    })
+    .catch(err => console.log('ERROR FRONT END', err))
+
+// console.log(res)
+
+//     const data = await res.json();
+    
+//     console.log(data)
+    
+//     // .then(res => {
+//     //   console.log('back at front end', Date.now());
+//     //   console.log(res)
+ 
+//     //   return res.json();
+//     //   }).then(data => {
+        
+//     //     console.log(data);
+//     //   } 
+
+//       } catch (err) {
+//       console.log('ERROR FRONT END')
+//       console.log(err)
+//         }
+      }
+      
+  
 
   render() {
     return <button onClick={this.handleQueryClick}>Query DB</button>;
--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ const PORT = 3000;
 // });
 
 const db = require('../db');
-// console.log(db);
+
+const queryController = require('../controllers/queryController.js');
 
 // app.use(express.static(path.resolve(__dirname, './client/src')));
 
@@ -22,78 +23,9 @@ const db = require('../db');
 //   res.status(200).sendFile(path.resolve(__dirname, '../client/src/index.html'));
 // });
 
-app.get('/query', async (req, res) => {
-  console.log('QUERYING');
-  const queryText =
-    'SELECT g.date, g.fen, g.termination, g.link, g.black, p.first_name as w_first_name, p.first_name as w_first_name, p.last_name as w_last_name, p.handle as w_handle, p.elo as w_elo from games g LEFT JOIN players p ON g.white = p._id;';
-    try {
-  let whiteRes = await db.query(queryText);
-  whiteRes = whiteRes.rows[0]
-
-    const blackId = whiteRes.black;
-    console.log({blackId});
-    delete whiteRes.black;
-    res.locals.gameData = whiteRes
-     const params = [blackId];
-     console.log(params);
-      const blackQuery =
-        'SELECT p.first_name as b_first_name, p.first_name as b_first_name, p.last_name as b_last_name, p.handle as b_handle, p.elo as b_elo FROM players p WHERE p._id = $1;';
-    console.log('white res', whiteRes);
-
-    let blackRes = await db.query(queryText, params);
-    console.log('black res', blackRes);
-
-    for (const item in blackRes.rows[0]) {
-            console.log(item, blackRes.rows[0][item]);
-            res.locals.gameData[item] = blackRes.rows[0][item];
-          }
-
-          console.log(res.locals.gameData);
-
-
-
-    } catch (err) {
-      console.log('ERROR', err);
-    }
-  
-  // , (err, queryRes) => {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log('Got White results!');
-  //     console.log(queryRes.rows);
-  //     res.locals.gameData = queryRes.rows[0];
-  //     return queryRes;
-  //   }
-  // });
-
-
-      // console.log('Locals first query', res.locals.gameData);
-  //     const blackId = queryRes.rows[0].black;
-  //     const params = [blackId];
-  //     const blackQuery =
-  //       'SELECT p.first_name as b_first_name, p.first_name as b_first_name, p.last_name as b_last_name, p.handle as b_handle, p.elo as b_elo FROM players p WHERE p._id = $1;';
-  //     pool.query(blackQuery, params, (err, queryRes) => {
-  //       if (err) console.log(err);
-  //       else {
-  //         // console.log('Locals second query', res.locals.gameData);
-  //         console.log('Got Black results!');
-  //         console.log(queryRes.rows);
-
-  //         for (const item in queryRes.rows[0]) {
-  //           console.log(item, queryRes.rows[0][item]);
-  //           res.locals.gameData[item] = queryRes.rows[0][item];
-  //         }
-  //         delete res.locals.gameData.black;
-  //         console.log('inside', res.locals.gameData);
-  //         const dataToSend = res.locals.gameData;
-  //         res.status(200).send(dataToSend);
-  //       }
-  //     })
-  // // console.log('outside',res.locals.gameData);
-  
-  //   }
-  // })
+app.get('/query', queryController.getGames, (req, res) => {
+  console.log('in final middleware');
+  return res.status(200).json(res.locals.gameData);
 
 });
 
@@ -108,6 +40,11 @@ app.use((err, req, res, next) => {
     status: 300,
     message: 'Global error handler',
   };
+  const newError = {
+    ...errorObj,
+    log,
+    message
+  }
   res.status(300).json(errorObj);
 });
 
